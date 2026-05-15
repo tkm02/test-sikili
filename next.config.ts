2dd2aacf8087dcc7af619e174e87afe30867737f
MAMADOU TUO <102806133+tkm02@users.noreply.github.com>
2026-05-15 21:30:34 +0000

    deploy4

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,13 @@ import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   output: 'standalone',
+  outputFileTracingExcludes: {
+    '*': [
+      'node_modules/@swc/**',
+      'node_modules/next/dist/compiled/**',
+      'node_modules/webpack/**',
+    ],
+  },
 }
 
 export default nextConfig
